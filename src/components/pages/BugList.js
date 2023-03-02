06c797632878fb6108830a24308930073848a67e
Grant Gonzalez <grantgonzalez14@gmail.com>
2023-03-02 14:20:11 -0500

    Created task input bar and button, beginning work on functionality

--- src/components/pages/BugList.js
+++ src/components/pages/BugList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from '../Button';
 import Navbar from '../Navbar';
 import './BugList.css';
 
@@ -9,7 +10,10 @@ function BugList() {
         <div className='bl-screen'>
             <div className='bl-tasks'>
                 <h1>TASKS</h1>
-                <input className='new-task-input' type='text' placeholder='Enter New Task' />
+                <div className='new-task-input-container'>
+                    <input className='new-task-input' type='text' placeholder='Enter New Task' />
+                    <Button buttonStyle='btn--task'>Create</Button>
+                </div>
             </div>
             <div className='bl-in-progress'></div>
             <div className='bl-done'></div>
