c91dd49d25b3dd78394d7b4e2aa6c98c8467f8f6
Grant Gonzalez <grantgonzalez14@gmail.com>
2023-03-02 16:45:52 -0500

    Added remove task button and dropdown menu to move task, beginning work on functionality for those

--- src/components/Task.js
+++ src/components/Task.js
@@ -8,6 +8,12 @@ function Task({task}) {
                 <ul>
                     <li>{task.title}</li>
                 </ul>
+                <select className='dropdown-menu'>
+                    <option value='Status' selected>To-Do</option>
+                    <option value='In Progress'>In Progress</option>
+                    <option value='Resolved'>Resolved</option>
+                </select>
+                <button className='btn-remove-task'>Remove</button>
             </div>
         </>
     )
