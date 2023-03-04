81eb6954e07d8dfac5009eb9971adaf3be47773d
Grant Gonzalez <grantgonzalez14@gmail.com>
2023-03-04 13:47:29 -0500

    Added validation checks on sign up page

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -6,19 +6,32 @@ import Navbar from '../Navbar';
 import './SignUp.css';
 
 function SignUp() {
+    const [registerName, setRegisterName] = useState('');
     const [registerEmail, setRegisterEmail] = useState('');
     const [registerPassword, setRegisterPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
-    const [registerName, setRegisterName] = useState('');
+    
     const navigate = useNavigate();
 
     const register = () => {
-        if (registerPassword === confirmPassword) {
-            registerWithEmailAndPassword(registerName, registerEmail, registerPassword);
-            navigate('/');
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+
+        if (!registerName || !registerEmail || !registerPassword || !confirmPassword) {
+            alert('All fields must be filled out to continue!');
+        }
+        else if (!registerEmail.includes('@') || !registerEmail.includes('.')) {
+            alert('You must register with a valid email!');
+        }
+        else if (!passwordRegex.test(registerPassword)) {
+            alert('Password must be at least 6 characters in length, contain at least one capital letter, contain at least one special character, and contain at least one digit!');
+        }
+        else if (registerPassword !== confirmPassword) {
+            alert('Passwords do not match!');
         }
         else { 
-            alert('Passwords do not match!'); 
+            registerWithEmailAndPassword(registerName, registerEmail, registerPassword);
+            alert('Sign up successful!');
+            navigate('/');
         }
     }
 
@@ -31,22 +44,22 @@ function SignUp() {
                     <form action=''>
                         <div className='form-input'>
                             <i className='ri-user-line'/>
-                            <input type='text' placeholder='Name' onChange={(event) => {setRegisterName(event.target.value);}}/>
+                            <input type='text' placeholder='Name' onChange={(event) => {setRegisterName(event.target.value);}} required/>
                             <span className='bar'/>
                         </div>
                         <div className='form-input'>
                             <i className='ri-mail-line'/>
-                            <input type='text' placeholder='Email' onChange={(event) => {setRegisterEmail(event.target.value);}}/>
+                            <input type='text' placeholder='Email' onChange={(event) => {setRegisterEmail(event.target.value);}} required/>
                             <span className='bar'/>
                         </div>
                         <div className='form-input'>
                             <i className='ri-lock-line'/>
-                            <input type='password' placeholder='Password' onChange={(event) => {setRegisterPassword(event.target.value);}}/>
+                            <input type='password' placeholder='Password' onChange={(event) => {setRegisterPassword(event.target.value);}} required/>
                             <span className='bar'/>
                         </div>
                         <div className='form-input'>
                             <i className='ri-lock-line'/>
-                            <input type='password' placeholder='Confirm Password' onChange={(event) => {setConfirmPassword(event.target.value);}}/>
+                            <input type='password' placeholder='Confirm Password' onChange={(event) => {setConfirmPassword(event.target.value);}} required/>
                             <span className='bar'/>
                         </div>
                         <button type='button' className='form-button' onClick={register}>Sign Up</button>
