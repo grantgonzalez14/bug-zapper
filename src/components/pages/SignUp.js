9044c74faafa2971cd5a12dfa7c202eb78c57b9e
Grant Gonzalez <grantgonzalez14@gmail.com>
2023-03-08 14:13:25 -0500

    Added password validation visuals, i.e. strength bar and necessary components of password creation that is reactive to whether the components are there or not

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { registerWithEmailAndPassword } from '../../Firebase';
 import { useNavigate } from 'react-router-dom';
@@ -10,8 +10,16 @@ function SignUp() {
     const [registerEmail, setRegisterEmail] = useState('');
     const [registerPassword, setRegisterPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [passwordStrength, setPasswordStrength] = useState('');
     
     const navigate = useNavigate();
+    const validations = [
+        (registerPassword.length > 0),
+        (registerPassword.length >= 6),
+        (registerPassword.search(/[A-Z]/) > -1),
+        (registerPassword.search(/[0-9]/) > -1),
+        (registerPassword.search(/[$&+,:;=?!@#*]/) > -1)
+    ]
 
     const register = () => {
         const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
@@ -35,6 +43,32 @@ function SignUp() {
         }
     }
 
+    useEffect(() => {
+        let strength;
+        let numValidations = validations.reduce((acc, cur) => acc + cur);
+
+        switch (numValidations) {
+            case 0:
+            case 1:
+            case 2:
+                strength = 'weak';
+                break;
+            case 3:
+            case 4:
+                strength = 'average';
+                break;
+            case 5:
+                strength = 'strong';
+                break;
+            default:
+                strength = 'weak';
+                break;
+        }
+        
+        setPasswordStrength(strength);
+
+    }, [registerPassword]); // eslint-disable-line react-hooks/exhaustive-deps
+
     return (
         <>
             <Navbar />
@@ -57,11 +91,25 @@ function SignUp() {
                             <input type='password' placeholder='Password' onChange={(event) => {setRegisterPassword(event.target.value);}} required/>
                             <span className='bar'/>
                         </div>
+                        {registerPassword !== '' &&
+                        <div className='password-strength-container'>
+                            <div className='password-strength-label'>Strength</div>
+                            <div className='password-strength'>
+                                <div className={passwordStrength}><span></span></div>
+                            </div>
+                        </div>
+                        }
                         <div className='form-input'>
                             <i className='ri-lock-line'/>
                             <input type='password' placeholder='Confirm Password' onChange={(event) => {setConfirmPassword(event.target.value);}} required/>
                             <span className='bar'/>
                         </div>
+                        <ul type='none'>
+                            <li>{validations[1] ? '✅' : '❌'} Password be at least 6 characters</li>
+                            <li>{validations[2] ? '✅' : '❌'} Password must contain a capital letter</li>
+                            <li>{validations[3] ? '✅' : '❌'} Password must contain a number</li>
+                            <li>{validations[4] ? '✅' : '❌'} Password must contain a special character</li>
+                        </ul>
                         <button type='button' className='form-button' onClick={register}>Sign Up</button>
                         <span className='form-switch'>
                             Already have an account?
